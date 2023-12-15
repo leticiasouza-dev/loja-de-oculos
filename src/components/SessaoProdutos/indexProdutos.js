43d69916b786e38499bbfe9561490a7157da5f6f
leticiaSouzaDev <lec667121@gmail.com>
2023-12-15 13:13:06 -0300

    :lipstick:feat: style responsive of component SessaoProdutos

--- src/components/SessaoProdutos/indexProdutos.js
+++ src/components/SessaoProdutos/indexProdutos.js
@@ -3,33 +3,34 @@ import './styleProdutos.css';
 
 export function SessaoProdutos(){
     return (
-        <section>
+        <section className='sessaoProdutos'>
             <h2>Nossos produtos</h2>
             <p>Trabalhamos com óculos de grau, óculos de sol, lentes transition nos modelos masculino, feminino e infantil.</p>
             <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado.</p>
             
-            <div>
-                <h4>Óculos de grau</h4>
-                <img src='/assets/imgs/oculos01.png' alt=''/>
-                <p>R$ 500,00</p>
-            </div>
+            <div className='container'>
+                <div>
+                    <h4>Óculos de grau</h4>
+                    <img src='/assets/imgs/oculos01.png' alt=''/>
+                    <p>R$ 500,00</p>
+                </div>
 
-            <div>
-                <h4>Óculos transition</h4>
-                <img src='/assets/imgs/oculos02.png' alt=''/>
-                <p>R$750,00</p>
-            </div>
+                <div>
+                    <h4>Óculos transition</h4>
+                    <img src='/assets/imgs/oculos02.png' alt=''/>
+                    <p>R$750,00</p>
+                </div>
 
-            <div>
-                <h4>Óculos de sol</h4>
-                <img src='/assets/imgs/oculos03.png' alt=''/>
-                <p>R$700,00</p>
-            </div>
-
-            <div>
-                <h4>Óculos infantil</h4>
-                <img src='/assets/imgs/oculos04.png' alt=''/>
-                <p>R$500,00</p>
+                <div className='oculos3'>
+                    <h4>Óculos de sol</h4>
+                    <img src='/assets/imgs/oculos03.png' alt='' />
+                    <p>R$700,00</p>
+                </div>
+                <div>
+                    <h4>Óculos infantil</h4>
+                    <img src='/assets/imgs/oculos04.png' alt=''/>
+                    <p>R$500,00</p>
+                </div>
             </div>
 
             <p>Todos os nossos produtos incluem:</p>
