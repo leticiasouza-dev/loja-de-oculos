972d0f53c00ac060ae2cfa588a0e39215bb45706
leticiaSouzaDev <lec667121@gmail.com>
2023-12-15 18:23:09 -0300

    :lipstick:feat: style responsive of component Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header/indexHeader.js';
 import Main from './components/Main/indexMain.js';
-
+import Footer from './components/Footer/indexFooter.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/indexFooter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styleFooter.css';
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; 2022 Óticas Vida - Todos os direitos reservados.</p>
+        </footer>
+    )
+}
+export default Footer;
